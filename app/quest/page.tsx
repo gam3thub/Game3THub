0703233fa0a464fc98f01583a69d6128470992bb
gam3thub <gam3thub@gmail.com>
2024-11-16 11:43:08 +0800

    [Jaya] Restore quest UI

--- app/quest/page.tsx
+++ app/quest/page.tsx
@@ -1,16 +1,16 @@
-//import Slider from '../../components/Slider';
-//import GameGallery from '../../components/GameGallery';
+import Slider from '../../components/Slider';
+import GameGallery from '../../components/GameGallery';
 import BottomBar from '../../components/BottomBar';
 
 export default function QuestPage() {
   return (
     <main className="max-w-4xl mx-auto">
       {
-        //<Slider />
+        <Slider />
       }
-      
+      This is Quest
       {
-        //<GameGallery />
+        <GameGallery />
       }
       <BottomBar />
     </main>
